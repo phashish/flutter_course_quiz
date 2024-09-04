32f3276dc5b362f0c9b893bf7b24d0543976a913
Ashish Disawal <ashish@prahari.net>
2024-09-04 22:53:19 +0530

    removed safearea

--- lib/screens/intro_screen.dart
+++ lib/screens/intro_screen.dart
@@ -6,39 +6,37 @@ class IntroScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.deepPurple.shade100,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Image(image: AssetImage('assets/images/quiz-nbg.png')),
-              const SizedBox(
-                height: 10,
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade100,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Image(image: AssetImage('assets/images/quiz-nbg.png')),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              'Learn Flutter the fun way!',
+              style: TextStyle(
+                fontSize: 24,
+                color: Colors.deepPurple.shade900,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                'Learn Flutter the fun way!',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.deepPurple.shade900,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple.shade800,
+                  foregroundColor: Colors.white),
+              onPressed: () => context.go('/quiz/1'),
+              child: const Text(
+                'Start Quiz',
               ),
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple.shade800,
-                    foregroundColor: Colors.white),
-                onPressed: () => context.go('/quiz/1'),
-                child: const Text(
-                  'Start Quiz',
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
--- lib/screens/results_screen.dart
+++ lib/screens/results_screen.dart
@@ -14,63 +14,80 @@ class ResultsScreen extends StatelessWidget {
     }
     String displayScore =
         "Score: ${totalScore.toString()} / ${answeredQuestions.length.toString()}";
-    return SafeArea(
-      child: Scaffold(
-        backgroundColor: Colors.deepPurple.shade100,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              const Image(image: AssetImage('assets/images/quiz-nbg.png')),
-              Text(displayScore, style: const TextStyle(fontSize: 24)),
-              ListView.separated(
-                padding: const EdgeInsets.all(50),
-                shrinkWrap: true,
-                itemCount: answeredQuestions.length,
-                separatorBuilder: (context, index) {
-                  return const Divider();
-                },
-                itemBuilder: (context, index) {
-                  String choosen = answeredQuestions[index].choosenAnswer;
-                  String correct = answeredQuestions[index].correctAnswer;
-                  String subText =
-                      "You chose: $choosen\nCorrect answer: $correct";
-                  return ListTile(
-                    tileColor:
-                        choosen == correct ? Colors.blue[100] : Colors.red[200],
-                    contentPadding:
-                        const EdgeInsets.only(left: 20.0, right: 20.0),
-                    shape: RoundedRectangleBorder(
-                      side: BorderSide(
-                        color: Colors.deepPurple.shade900,
-                        width: 2,
-                      ),
-                      borderRadius: BorderRadius.circular(10),
+    return Scaffold(
+      backgroundColor: Colors.deepPurple.shade100,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Image(image: AssetImage('assets/images/quiz-nbg.png')),
+            Text(displayScore, style: const TextStyle(fontSize: 24)),
+            ListView.separated(
+              padding: const EdgeInsets.all(50),
+              shrinkWrap: true,
+              itemCount: answeredQuestions.length,
+              separatorBuilder: (context, index) {
+                return const Divider();
+              },
+              itemBuilder: (context, index) {
+                String choosen = answeredQuestions[index].choosenAnswer;
+                String correct = answeredQuestions[index].correctAnswer;
+                String subText =
+                    "You chose: $choosen\nCorrect answer: $correct";
+                return ListTile(
+                  tileColor:
+                      choosen == correct ? Colors.blue[100] : Colors.red[200],
+                  contentPadding:
+                      const EdgeInsets.only(left: 20.0, right: 20.0),
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.deepPurple.shade900,
+                      width: 2,
                     ),
-                    title: Text(
-                      answeredQuestions[index].question,
-                      // textAlign: TextAlign.center,
-                    ),
-                    subtitle: Text(
-                      subText,
-                      // textAlign: TextAlign.center,
-                    ),
-                  );
-                },
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  title: Text(
+                    answeredQuestions[index].question,
+                    // textAlign: TextAlign.center,
+                  ),
+                  subtitle: Text(
+                    subText,
+                    // textAlign: TextAlign.center,
+                  ),
+                );
+              },
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.deepPurple.shade800,
+                  foregroundColor: Colors.white),
+              onPressed: () => context.go('/'),
+              child: const Text(
+                'Go to start',
               ),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurple.shade800,
-                    foregroundColor: Colors.white),
-                onPressed: () => context.go('/'),
-                child: const Text(
-                  'Go to start',
-                ),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class ItemWidget extends StatelessWidget {
+  const ItemWidget({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: SizedBox(
+        height: 100,
+        child: Center(child: Text(text)),
       ),
     );
   }
